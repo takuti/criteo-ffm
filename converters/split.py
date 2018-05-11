50a456409dd4450ebe01c76f10ed296b3dab0369
Takuya Kitazawa <k.takuti@gmail.com>
2018-05-12 07:33:24 +0900

    Fix wrong target file stores split samples

--- converters/split.py
+++ converters/split.py
@@ -12,9 +12,9 @@ f_va = open(output_file_va, 'w')
 
 for line in open(input_file):
     if random.random() < 0.2:
-        f_tr.write(line)
-    else:
         f_va.write(line)
+    else:
+        f_tr.write(line)
 
 f_tr.close()
 f_va.close()
