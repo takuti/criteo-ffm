6b8fa118921ce840e8c42ba0f130dd3f59ce456c
Takuya Kitazawa <k.takuti@gmail.com>
2018-05-11 14:33:17 +0900

    Fix preprocess to change data format for libffm

--- preprocess.sh
+++ preprocess.sh
@@ -5,3 +5,9 @@ utils/count.py tr.csv > fc.trva.t10.txt
 thread_num=10
 converters/parallelizer-b.py -s ${thread_num} converters/pre-b.py tr.csv tr.ffm
 converters/parallelizer-b.py -s ${thread_num} converters/pre-b.py te.csv te.ffm
+
+converters/change_format.py tr.ffm tr.ffm.formatted
+converters/change_format.py te.ffm te.ffm.formatted
+
+mv tr.ffm.formatted tr.ffm
+mv te.ffm.formatted te.ffm
