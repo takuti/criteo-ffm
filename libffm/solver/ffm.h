5d1a1eb2000b7903249fb99f6373be64ce7bb62d
Takuya Kitazawa <k.takuti@gmail.com>
2018-05-16 16:07:50 +0900

    Format LIBFFM code

--- libffm/solver/ffm.h
+++ libffm/solver/ffm.h
@@ -13,28 +13,28 @@ typedef int ffm_int;
 typedef long long ffm_long;
 
 struct ffm_node {
-    ffm_int f; // field index
-    ffm_int j; // feature index
-    ffm_float v; // value
+  ffm_int f;    // field index
+  ffm_int j;    // feature index
+  ffm_float v;  // value
 };
 
 struct ffm_model {
-    ffm_int n; // number of features
-    ffm_int m; // number of fields
-    ffm_int k; // number of latent factors
-    ffm_float *W = nullptr;
-    bool normalization;
-    ~ffm_model();
+  ffm_int n;  // number of features
+  ffm_int m;  // number of fields
+  ffm_int k;  // number of latent factors
+  ffm_float *W = nullptr;
+  bool normalization;
+  ~ffm_model();
 };
 
 struct ffm_parameter {
-    ffm_float eta = 0.2; // learning rate
-    ffm_float lambda = 0.00002; // regularization parameter
-    ffm_int nr_iters = 15;
-    ffm_int k = 4; // number of latent factors
-    bool normalization = true;
-    bool cumulative_loss = false;
-    bool auto_stop = false;
+  ffm_float eta = 0.2;         // learning rate
+  ffm_float lambda = 0.00002;  // regularization parameter
+  ffm_int nr_iters = 15;
+  ffm_int k = 4;  // number of latent factors
+  bool normalization = true;
+  bool cumulative_loss = false;
+  bool auto_stop = false;
 };
 
 void ffm_read_problem_to_disk(string txt_path, string bin_path);
@@ -43,10 +43,11 @@ void ffm_save_model(ffm_model &model, string path);
 
 ffm_model ffm_load_model(string path);
 
-ffm_model ffm_train_on_disk(string Tr_path, string Va_path, ffm_parameter param);
+ffm_model ffm_train_on_disk(string Tr_path, string Va_path,
+                            ffm_parameter param);
 
 ffm_float ffm_predict(ffm_node *begin, ffm_node *end, ffm_model &model);
 
-} // namespace ffm
+}  // namespace ffm
 
-#endif // _LIBFFM_H
+#endif  // _LIBFFM_H
--- libffm/solver/timer.cpp
+++ libffm/solver/timer.cpp
@@ -1,31 +1,20 @@
 #include <string>
 #include "timer.h"
 
-Timer::Timer()
-{
-    reset();
+Timer::Timer() { reset(); }
+
+void Timer::reset() {
+  begin = std::chrono::high_resolution_clock::now();
+  duration =
+      std::chrono::duration_cast<std::chrono::milliseconds>(begin - begin);
 }
 
-void Timer::reset()
-{
-    begin = std::chrono::high_resolution_clock::now();
-    duration = 
-        std::chrono::duration_cast<std::chrono::milliseconds>(begin-begin);
+void Timer::tic() { begin = std::chrono::high_resolution_clock::now(); }
+
+float Timer::toc() {
+  duration += std::chrono::duration_cast<std::chrono::milliseconds>(
+      std::chrono::high_resolution_clock::now() - begin);
+  return get();
 }
 
-void Timer::tic()
-{
-    begin = std::chrono::high_resolution_clock::now();
-}
-
-float Timer::toc()
-{
-    duration += std::chrono::duration_cast<std::chrono::milliseconds>
-                    (std::chrono::high_resolution_clock::now()-begin);
-    return get();
-}
-
-float Timer::get()
-{
-    return (float)duration.count() / 1000;
-}
+float Timer::get() { return (float)duration.count() / 1000; }
