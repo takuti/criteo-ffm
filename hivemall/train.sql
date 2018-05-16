bc241d062b0165c16868f94148f078809516e032
Takuya Kitazawa <k.takuti@gmail.com>
2018-05-16 15:28:52 +0900

    Use closer `train_ffm` options to what LIBFFM does
    * V is initialized by uniform distribution which generates a value in
    [0.0, 1.0)
    * LIBFFM does not use the liner terms

--- hivemall/train.sql
+++ hivemall/train.sql
@@ -8,7 +8,7 @@ CREATE TABLE  criteo.ffm_model (
 
 INSERT OVERWRITE TABLE criteo.ffm_model
 SELECT
-  train_ffm(features, label, '-init_v random -classification -iterations 15 -factors 4 -eta 0.2 -l2norm -optimizer sgd -lambda 0.00002 -cv_rate 0.0')
+  train_ffm(features, label, '-init_v random -max_init_value 1.0 -classification -iterations 15 -factors 4 -eta 0.2 -l2norm -optimizer sgd -lambda 0.00002 -cv_rate 0.0 -disable_wi')
 FROM (
   SELECT
     features, label
